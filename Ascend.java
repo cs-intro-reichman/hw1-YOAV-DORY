c7271e3e1930a2a44e44a3465afca0cb0e666a4f
YOAV-DORY <yoav.dory@post.runi.ac.il>
2025-10-30 09:33:15 +0200

    completed

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,21 @@
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
 public class Ascend {
-	public static void main(String[] args) {
-		// Replace this comment with your code
+	public static void main(String[] args)
+	{
+		int num = Integer.parseInt(args[0]);
+
+		double r = Math.random();
+		double t = Math.random();
+		double s = Math.random();
+
+		int a = (int) (r * num);
+		int b = (int) (t * num);
+		int c = (int) (s * num);
+
+		int min = Math.min(Math.min(a,b),c);
+		int max = Math.max(Math.max(a,b),c);
+		int Ascend = a + b + c - min - max;
+		System.out.println(min +" "+ Ascend + " " + max);
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,10 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int currenvalue = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1]) / 100;
+		int years = Integer.parseInt(args[2]);
+		double futurevalue = currenvalue * (Math.pow((1 + rate), years));
+		System.out.println("After " + years + " years, $" + currenvalue + " saved at - " + Math.floor(rate*100) + "% will yeild - $" + Math.floor(futurevalue));
 	}
 }
